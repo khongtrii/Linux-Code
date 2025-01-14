cd43a13233e88f2e555255542d56402774f95b6f
khongtri <trikhong2004@gmail.com>
2025-01-14 17:28:31 +0700

    added

--- latent diffusion/AutoEncoder.py
+++ latent diffusion/AutoEncoder.py
@@ -1,4 +1,4 @@
-""""""
+""" - Library - """
 from typing import List
 
 import torch
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 """"""
 
-""""""
+""" - Activation - """
 def swish(x:torch.Tensor):
     return x*torch.sigmoid(x)
 
@@ -14,7 +14,7 @@ def normalization(channels: int):
     return nn.GroupNorm(num_groups=32, num_channels=channels, eps=1e-6)
 """"""
 
-""""""
+""" - Resnet - """
 class ResnetBlock(nn.Module):
     def __init__(self, in_channels:int, out_channels:int):
         super().__init__()
@@ -44,4 +44,21 @@ class ResnetBlock(nn.Module):
         return self.nin_shortcut(x) + h
 """"""
 
+""" - Down/Up Sampling - """
+class UpSample(nn.Module):
+    def __init__(self, channels: int):
+        super().__init__()
+        self.conv == nn.Conv2d(channels, channels, 3, padding=1)
+    def forward(self, x: torch.Tensor):
+        x = F.interpolate(x, scale_factor=2.0, mode="nearest")
+        return self.conv(x)
+    
+class DownSample(nn.Module):
+    def __init__(self, channels:int):
+        super().__init__()
+        self.conv = nn.Conv2d(channels, channels, 3, stride=2, padding=4)
+    def forward(self, x:torch.Tensor):
+        x = F.pad(x, (0, 1, 0, 1), mode="constant", value=0)
+        return self.conv(x)
+""""""
 
